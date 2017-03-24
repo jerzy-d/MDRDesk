a49caa37ba6c51c809437e1a61a59611e04cb70c
jdominczak <jdominczak@DOMINCZAK9020>
2017-03-24 16:00:01 -0400

    Type value report, starting new approach...

---
+++ ClrMDRUtil/ClrUtils/HeapTypeInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Diagnostics.Runtime;
+
+namespace ClrMDRIndex
+{
+    public class HeapTypeInfo
+    {
+        private ClrType _clrType;
+        public ClrType Type => _clrType;
+        private ClrInstanceField _field;
+        public ClrInstanceField Field => _field;
+
+        private HeapTypeInfo _parent;
+        public HeapTypeInfo Parent => _parent;
+        private HeapTypeInfo[] _children;
+        public HeapTypeInfo[] Children => _children;
+
+
+    }
+}
