a1d18273dbc44c1c18f628d17be0e808c597b77e
jdominczak <jdominczak@DOMINCZAK9020>
2017-06-02 16:04:45 -0400

    Fix of valid path chars...

--- ClrMDRUtil/ClrUtils/DumpFileMoniker.cs
+++ ClrMDRUtil/ClrUtils/DumpFileMoniker.cs
@@ -208,7 +208,53 @@ namespace ClrMDRIndex
 			return System.IO.Path.GetFileName(path) + ".map";
 		}
 
-        public static HashSet<char> InvalidPathChars = new HashSet<char>(System.IO.Path.GetInvalidPathChars());
+        /// <summary>
+        /// Got that from System.IO.Path.GetInvalidPathChars(), and added some.
+        /// </summary>
+        public static char[] InvalidPathCharsAry = new char[]
+        {
+            '"',
+            '<',
+            '>',
+            '|',
+            '\0',
+            '\u0001',
+            '\u0002',
+            '\u0003',
+            '\u0004',
+            '\u0005',
+            '\u0006',
+            '\a',
+            '\b',
+            '\t',
+            '\n',
+            '\v',
+            '\f',
+            '\r',
+            '\u000e',
+            '\u000f',
+            '\u0010',
+            '\u0011',
+            '\u0012',
+            '\u0013',
+            '\u0014',
+            '\u0015',
+            '\u0016',
+            '\u0017',
+            '\u0018',
+            '\u0019',
+            '\u001a',
+            '\u001b',
+            '\u001c',
+            '\u001d',
+            '\u001e',
+            '\u001f',
+            ':',
+            '/',
+            '\\'
+        };
+
+        public static HashSet<char> InvalidPathChars = new HashSet<char>(InvalidPathCharsAry);
 
         public static string GetValidFileName(string name)
         {
